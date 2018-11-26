5606ef1917833db9e4a58d699504fb9ea764eb6e
Brock Fanning <brockfanning@gmail.com>
2018-11-26 18:47:48 -0500

    Fix repo URL

--- scripts/deploy/circleci/deploy.sh
+++ scripts/deploy/circleci/deploy.sh
@@ -3,11 +3,7 @@
 git config --global user.email $GH_EMAIL
 git config --global user.name $GH_NAME
 
-# CircleCI will identify the SSH key with a "Host" of gh-stg. In order to tell
-# Git to use this key, we need to hack the SSH key:
-sed -i -e 's/Host gh-stg/Host gh-stg\n  HostName github.com/g' ~/.ssh/config
-# Now we can use "gh-stg" below to identify the SSH key to use.
-git clone git@gh-stg:$GH_ORG_STG/$CIRCLE_PROJECT_REPONAME.git out
+git clone git@github.com:$GH_ORG/$CIRCLE_PROJECT_REPONAME.git out
 
 cd out
 git checkout gh-pages || git checkout --orphan gh-pages
